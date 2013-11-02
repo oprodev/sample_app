c9ea6f615d7477d9bc7dbf7ad24f53f92065043d
Oleg Prosalov <oprodev@gmail.com>
2013-11-02 00:09:41 -0400

    Replace sqlite3 with PostgreSQL

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ ruby '2.0.0'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.0'
+gem 'pg', '0.15.1'
 gem 'bootstrap-sass', '2.3.2.0'
 gem 'bcrypt-ruby', '3.0.1'
 gem 'faker', '1.1.2'
@@ -12,13 +13,12 @@ gem 'bootstrap-will_paginate', '0.0.9'
 
 # Use sqlite3 as the database for Active Record
 group :development, :test do 
-	gem 'sqlite3', '1.3.8'
+	#gem 'sqlite3', '1.3.8'
 	gem 'rspec-rails', '2.13.1'
-  # The following optional lines are part of the advanced setup.
-  # gem 'guard-rspec', '2.5.0'
-  # gem 'spork-rails', '4.0.0'
-  # gem 'guard-spork', '1.5.0'
-  # gem 'childprocess', '0.3.6'
+  gem 'guard-rspec', '2.5.0'  
+  gem 'spork-rails', '4.0.0'
+  gem 'guard-spork', '1.5.0'
+  gem 'childprocess', '0.3.6'
 end
 
 group :test do
@@ -32,7 +32,7 @@ group :test do
   # gem 'growl', '1.0.3'
 
   # Uncomment these lines on Linux.
-  # gem 'libnotify', '0.8.0'
+   gem 'libnotify', '0.8.0'
 
   # Uncomment these lines on Windows.
   # gem 'rb-notifu', '0.0.4'
@@ -67,7 +67,6 @@ group :doc do
 end
 
 group :production do
-	gem 'pg', '0.15.1'
 	gem 'rails_12factor', '0.0.2'
 end
 
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe "Static pages" do
+	let(:base_title) {"Ruby on Rails Tutorial Sample App"}
+
   # Home page
   describe "Home page" do
     it "should have the content 'Sample App'" do
@@ -10,7 +12,7 @@ describe "Static pages" do
 
     it "should have the title 'Home'" do
     	visit '/static_pages/home'
-    	expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+    	expect(page).to have_title("#{base_title} | Home")
     end
   end
 
@@ -23,7 +25,7 @@ describe "Static pages" do
 
   	it "should have the title 'Help'" do
     	visit '/static_pages/help'
-    	expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+    	expect(page).to have_title("#{base_title} | Help")
     end
   end
 
@@ -36,7 +38,7 @@ describe "Static pages" do
 
   	it "should have the title 'About'" do
     	visit '/static_pages/about'
-    	expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
+    	expect(page).to have_title("#{base_title} | About")
     end
   end
 
@@ -44,7 +46,7 @@ describe "Static pages" do
   describe "Contact page" do
   	it "should have the title 'Contact'" do
     	visit '/static_pages/contact'
-    	expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
+    	expect(page).to have_title("#{base_title} | Contact")
     end
   end
 end
